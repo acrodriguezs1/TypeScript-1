d30179716e5b94068ea7c9ccf1887310d3285002
acrodriguezs1 <123606873+acrodriguezs1@users.noreply.github.com>
2024-04-13 12:56:25 -0500

    uplode final

---
+++ serie.ts
@@ -0,0 +1,12 @@
+export class serie{
+    constructor(public numero:number,
+         public nombre:string, 
+         public channel:string, 
+         public seasons:number, 
+         public descropcion: string, 
+         public url:string, 
+         public imagen:string)
+    {
+
+    }
+}
